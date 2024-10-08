0048fad5aa59cf8df06705e9d9abdf2e5458924f
EmmaInverse <emma225h@gmail.com>
2024-10-07 21:37:43 -0300

    push cambio front

---
+++ front/src/components/navbar/navbar.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+import React from "react";
+
+const navbar = () => {
+  return (
+    <div>
+      <Link href={"/"}> Home </Link>
+    </div>
+  );
+};
+
+export default navbar;
