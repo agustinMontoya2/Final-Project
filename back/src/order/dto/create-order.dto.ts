31a316e4f7678d9d07b4ea6d6f6a4d5dc9c4ad30
Facumar04 <facumarrr33@gmail.com>
2024-10-14 20:02:15 -0300

    Creating dtos

--- back/src/order/dto/create-order.dto.ts
+++ back/src/order/dto/create-order.dto.ts
@@ -1,9 +1,27 @@
+import { IsArray, IsString } from 'class-validator';
+import { ProductDetail } from 'src/products/entities/productDetail.entity';
+
 export class CreateOrderDto {
+  @IsString()
   userId: string;
-  order_type: string;
-  payment_method: string;
-  //array []
-  productId: string;
-  quantity: number;
-  note: string;
+
+  @IsString()
+  order_type: OrderType;
+
+  payment_method: PaymentMethod;
+  // product: { product_id: string; quantity: number; note: string }[];
+
+  @IsArray()
+  products: ProductDetail[];
+}
+
+export enum OrderType {
+  TakeAway = 'take_away',
+  Delivery = 'delivery',
+}
+
+export enum PaymentMethod {
+  Cash = 'cash',
+  Card = 'card',
+  // agregar mp
 }
--- back/src/products/dto/create-product.dto.ts
+++ back/src/products/dto/create-product.dto.ts
@@ -1,9 +1,21 @@
 import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsBoolean,
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+  Length,
+} from 'class-validator';
 
 export class CreateProductDto {
+  @IsString({ message: 'product name must be a string' })
+  @IsNotEmpty({ message: 'product name is required' })
   @ApiProperty({
     description: 'Name of the product',
     example: 'Cheese burguer',
+  })
+  @Length(3, 50, {
+    message: 'Product name must be between 3 and 50 characters',
   })
   name: string;
 
@@ -11,12 +23,19 @@ export class CreateProductDto {
     description: 'Description of the product',
     example: 'The biggest cheeseburguer in the world',
   })
+  @IsNotEmpty({ message: 'product description is required' })
+  @IsString({ message: 'product description must be a string' })
+  @Length(3, 50, {
+    message: 'Product description must be between 3 and 50 characters',
+  })
   description: string;
 
   @ApiProperty({
     description: 'Price of the product',
     example: 199.99,
   })
+  @IsNumber({}, { message: 'product price must be a number' })
+  @IsNotEmpty({ message: 'product price is required' })
   price: number;
 
   @ApiProperty({
@@ -24,11 +43,16 @@ export class CreateProductDto {
     example:
       'https://res.cloudinary.com/dxpxzcj2i/image/upload/v1724243935/gvmpxhbyz3rvdsvnhvhm.webp',
   })
+  @IsString({
+    message: 'product image url must be a string',
+  })
   imgUrl: string;
 
   @ApiProperty({
     description: 'Availability of the product',
     example: true,
   })
+  @IsNotEmpty({message: 'Availability is required'})
+  @IsBoolean()
   available: boolean;
 }
