edcc8836498c7e1e985493cecf884f48684a4d09
federicogvaldez <federico.valdez@hotmail.com>
2024-10-17 12:41:51 +0200

    auth0

--- back/src/main.ts
+++ back/src/main.ts
@@ -2,9 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
+import { auth } from 'express-openid-connect';
+import { config as auth0Config } from './config/auth0.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(auth(auth0Config));
   app.useGlobalPipes(new ValidationPipe());
   const config = new DocumentBuilder()
     .setTitle('Restaurant Order Management API')
