005e875d5630b540a2b3cbf2ef7033ffe3fd48e3
Jehiel Lederhos <lederhos.jehiel@icloud.com>
2024-10-20 05:08:02 -0300

    recupere el front que yo mismo perdi

--- Front/src/components/Cards/Cards.tsx
+++ Front/src/components/Cards/Cards.tsx
@@ -5,12 +5,37 @@ import { IProducts } from "@/interfaces/productoInterface";
 import Image from "next/image";
 import Link from "next/link";
 import { useEffect, useState } from "react";
+import { addFavorities, removeFavorities } from "@/lib/server/favorities";
+import { addCart } from "@/lib/server/cart";
 
 const Cards = () => {
     const [products, setProducts] = useState<IProducts[]>([]);
     const [loading, setLoading] = useState(true);
     const [filter, setFilter] = useState<string>("");
     const [searchTerm, setSearchTerm] = useState<string>("");
+    const [userId, setUserId] = useState<string | null>(null);
+    const [ token, setToken] = useState<string | null>(null);
+    const [favorities, setFavorities] = useState<string[]>([]);
+    const [cart, setCart] = useState<string[]>([]);
+
+    useEffect(() => {
+        const storedUserData = window.localStorage.getItem("userSession");
+        if (storedUserData) {
+            const parsedData = JSON.parse(storedUserData);
+            if (parsedData && parsedData.user) {
+                setUserId(parsedData.user.user_id); 
+            }
+        }
+    }, []);
+    useEffect(() => {
+        const storedUserData = window.localStorage.getItem("userSession");
+        if (storedUserData) {
+            const parsedData = JSON.parse(storedUserData);
+            if (parsedData && parsedData.user) {
+                setToken(parsedData.token); 
+            }
+        }
+    }, []);
 
     useEffect(() => {
         const fetchProducts = async () => {
@@ -27,9 +52,37 @@ const Cards = () => {
         fetchProducts();
     }, []);
 
-    const handleAddToCart = async () => {
-        console.log("se hizo click");
-        // Aquí va la lógica de agregar al carrito
+    const handleAddToFavorities = async (productId: string, isFavorited: boolean) => {
+        if (token && userId) {
+            try {
+                if (isFavorited) {
+                    await removeFavorities(userId, productId, token);
+                    setFavorities((prevFavorities) => prevFavorities.filter((id) => id !== productId));
+                    console.log("Product has been eliminated");
+                } else {
+                    await addFavorities(userId, productId, token);
+                    setFavorities((prevFavorities) => [...prevFavorities, productId]);
+                }
+            } catch (error) {
+                console.error("Error al agregar favoritos", error.message);
+            }
+        } else {
+            alert("Inicia sesión para agregar favoritos.");
+        }
+    };
+
+    const handleAddCart = async (productId: string,) => {
+        if (token && userId) {
+            try {
+                    await addCart(userId, productId, token);
+                    setCart((prevCart) => [...prevCart, productId]);
+                    alert ("Producto agregado al carrito");
+            } catch (error) {
+                console.error("Error al agregar al carrito", error.message);
+            }
+        } else {
+            alert("Inicia sesión para agregar al carrito.");
+        }
     };
 
     const filteredProducts = products.filter((product) => {
@@ -53,7 +106,7 @@ const Cards = () => {
                     className="border border-gray-300 rounded-md px-3 py-1 text-gray-700"
                 />
             </div>
-            
+
             <div className="flex justify-center mb-4">
                 <button onClick={() => setFilter("Beverages")} className="mx-2 bg-secondary text-white py-1 px-3 rounded">Beverages</button>
                 <button onClick={() => setFilter("Main Dishes")} className="mx-2 bg-secondary text-white py-1 px-3 rounded">Main Dishes</button>
@@ -64,40 +117,53 @@ const Cards = () => {
             </div>
 
             <div className="w-[80%] h-auto flex flex-wrap justify-evenly m-auto">
-                {filteredProducts.map((product) => (
-                    <Link href={`/product/${product.product_id}`} key={product.product_id} className="w-[30%] h-44 flex items-center bg-primary shadow-2xl rounded-xl my-6 px-5 hover:scale-105 duration-500">
-                        <div className="w-1/2">
-                            <div className="relative w-36 h-36">
-                                <Image
-                                    src={product.image_url}
-                                    alt={product.product_name}
-                                    layout="fill"
-                                    objectFit="contain"
-                                    className="w-full h-auto"
-                                />
-                            </div>
-                        </div>
-                        <div className="w-1/2">
-                            <div>
-                                <h2 className="text-black text-xl font-semibold">{product.product_name}</h2>
-                                <p className="w-full text-black text-sm line-clamp-2">
-                                    <b>Description:</b>
-                                    {product.description}
-                                </p>
-                            </div>
-                            <div className="w-full flex justify-between items-center z-50">
-                                <p className="text-black text-sm"><b>Price:</b> ${product.price}</p>
-                                <button
-                                    className="bg-secondary px-3 py-1 rounded-md hover:bg-red-700"
-                                    onClick={handleAddToCart}
-                                >
-                                    Cart
-                                </button>
-                            </div>
-                        </div>
-                    </Link>
-                ))}
+    {filteredProducts.map((product) => (
+        <Link href={`/product/${product.product_id}`} key={product.product_id} className="w-[30%] h-44 flex items-center bg-primary shadow-2xl rounded-xl my-6 px-5 hover:scale-105 duration-500">
+            <div className="w-1/2">
+                <div className="relative w-36 h-36">
+                    <Image
+                        src={product.image_url}
+                        alt={product.product_name}
+                        layout="fill"
+                        objectFit="contain"
+                        className="w-full h-auto"
+                    />
+                </div>
             </div>
+            <div className="w-1/2">
+                <div>
+                    <h2 className="text-black text-xl font-semibold">{product.product_name}</h2>
+                    <p className="w-full text-black text-sm line-clamp-2">
+                        <b>Description:</b> {product.description}
+                    </p>
+                </div>
+                <div className="w-full flex justify-between items-center z-50">
+                    <p className="text-black text-sm"><b>Price:</b> ${product.price}</p>
+
+                    <button
+                        className="bg-secondary px-3 py-1 rounded-md hover:bg-red-700"
+                        onClick={(e) => {
+                            e.stopPropagation();  // Evita que el clic se propague al enlace
+                            handleAddCart(product.product_id);
+                        }}
+                    >
+                        Agregar a Carrito
+                    </button>
+
+                    <button
+                        className="bg-secondary px-3 py-1 rounded-md hover:bg-red-700"
+                        onClick={(e) => {
+                            e.stopPropagation();  // Evita que el clic se propague al enlace
+                            handleAddToFavorities(product.product_id, favorities.includes(product.product_id));
+                        }}
+                    >
+                        {favorities.includes(product.product_id) ? "Eliminar de Favoritos" : "Agregar a Favoritos"}
+                    </button>
+                </div>
+            </div>
+        </Link>
+    ))}
+</div>
         </div>
     );
 };
