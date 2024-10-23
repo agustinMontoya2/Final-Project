ad75e9ee8ed28f9bb8d5542083a154d70aed5abd
EmmaInverse <emma225h@gmail.com>
2024-10-23 13:15:59 -0300

    dashboard de admin

---
+++ Front/src/app/dashboardAdmind/viewReviews/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const allRevies = () => {
+    return (
+        <div><p>
+            acá va la logica para ver todas las reviews y poder borralas.
+        </p></div>
+    )
+}
+
+export default allRevies
