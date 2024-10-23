bcf7cf8c4e249fccca60656366ce34a8251ab81c
agustinMontoya2 <agus7_montoya@hotmail.com>
2024-10-23 18:07:39 -0300

    all reviews update user

--- back/src/users/users.controller.ts
+++ back/src/users/users.controller.ts
@@ -7,6 +7,7 @@ import {
   ParseUUIDPipe,
   Put,
   Post,
+  BadRequestException,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dto/update-user.dto';
