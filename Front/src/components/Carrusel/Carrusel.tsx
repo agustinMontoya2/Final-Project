a1c0f01d3be2c73c6cb5bcb3e3b048be5fe19508
Jehiel Lederhos <lederhos.jehiel@icloud.com>
2024-10-25 12:00:00 -0300

    carrusel

--- Front/src/components/Carrusel/Carrusel.tsx
+++ Front/src/components/Carrusel/Carrusel.tsx
@@ -2,50 +2,47 @@ import { useEffect, useState } from "react";
 import Image from "next/image";
 
 const Carousel = ({ images }) => {
-  const [currentIndex, setCurrentIndex] = useState(0);
+const [currentIndex, setCurrentIndex] = useState(0);
 
-  // Configurar desplazamiento automático cada 3 segundos
-  useEffect(() => {
+useEffect(() => {
     const interval = setInterval(() => {
-      setCurrentIndex((prevIndex) =>
+    setCurrentIndex((prevIndex) =>
         prevIndex === images.length - 1 ? 0 : prevIndex + 1
-      );
-    }, 3000); // 3000 ms = 3 segundos
+    );
+    }, 7000); // 3000 ms = 3 segundos
 
-    return () => clearInterval(interval); // Limpiar el intervalo al desmontar
-  }, [images.length]);
+    return () => clearInterval(interval); 
+}, [images.length]);
 
-  return (
+return (
     <div className="relative w-full max-w-lg mx-auto overflow-hidden rounded-lg shadow-lg">
-      <div
-        className="flex transition-transform duration-500 ease-in-out"
-        style={{ transform: `translateX(-${currentIndex * 100}%)` }}
-    >
-        {images.map((image, index) => (
-        <Image
-            key={index}
-            src={image}
-            alt={`Slide ${index + 1}`}
-            width={500}
-            height={300}
-            className="w-full object-cover"
-          />
-        ))}
-      </div>
+            <div
+                className="flex transition-transform duration-500 ease-in-out"
+                style={{ transform: `translateX(-${currentIndex * 100}%)` }}>
+                {images.map((image, index) => (
+                <Image
+                key={index}
+                src={image}
+                alt={`Slide ${index + 1}`}
+                width={800}
+                height={200}
+                className="w-full object-cover"
+                    />
+                ))}
+            </div>
 
-      {/* Indicadores de navegación */}
-      <div className="absolute bottom-3 left-1/2 transform -translate-x-1/2 flex space-x-2">
-        {images.map((_, index) => (
-          <div
-            key={index}
-            className={`h-2 w-2 rounded-full ${
-              index === currentIndex ? "bg-white" : "bg-gray-400"
-            }`}
-          ></div>
-        ))}
-      </div>
-    </div>
-  );
+            <div className="absolute bottom-3 left-1/2 transform -translate-x-1/2 flex space-x-2">
+                {images.map((_, index) => (
+                    <div
+                    key={index}
+                    className={`h-2 w-2 rounded-full ${
+                    index === currentIndex ? "bg-white" : "bg-gray-400"
+                    }`}
+                    ></div>
+                ))}
+            </div>
+        </div>
+    );
 };
 
 export default Carousel;
