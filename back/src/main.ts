747b4396fcf26d0b34f6f3e613eb47d9a8316aef
Santi Celis <santiagocelis2004@gmail.com>
2024-10-30 20:56:45 -0300

    mergin to main

--- back/src/main.ts
+++ back/src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:4000',
+    origin: 'https://final-project-nxrka547e-agustinmontoya2s-projects.vercel.app/', 
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
