22d59b32db1fdf0aaeb2922e221113ff4162218f
agustinMontoya2 <agus7_montoya@hotmail.com>
2024-10-30 20:55:52 -0300

    cleaning main

--- back/src/auth/auth.repository.ts
+++
@@ -1,7 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import { UsersRepository } from 'src/users/users.repository';
-
-@Injectable()
-export class AuthRepository {
-  constructor(private readonly usersRepository: UsersRepository) {}
-}
