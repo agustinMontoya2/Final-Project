ecf9a54c5169a2399d9db8fee5a49d42c75b1bad
EmmaInverse <emma225h@gmail.com>
2024-10-30 12:27:23 -0300

    arreglando TODO

--- Front/src/app/dashboardAdmin/page.tsx
+++ Front/src/app/dashboardAdmin/page.tsx
@@ -7,33 +7,20 @@ import AuthValidation from '@/hooks/AuthValidation';
 
 const DashboardAdmind = () => {
   const router = useRouter();
-  const esAdmind= AuthValidation()
+  const esAdmind = AuthValidation()
 
-  
-  const [admin, setAdmin] = React.useState(undefined);
-if(esAdmind){
-  return <div> redirigiendo a menu...</div>
-}
-  
-useEffect(() => {
-  const storedUserData = window.localStorage.getItem("userSession");
-  const parsedData = storedUserData ? JSON.parse(storedUserData) : null;
 
-  // Si parsedData y parsedData.user existen, se establece el estado de admin
-  if (parsedData?.user) {
-      setAdmin(parsedData.isAdmin);
+  if (esAdmind) {
+    return <div> redirigiendo a menu...</div>
   }
-}, []);
+  router.push("/menu")
 
-useEffect(() => {
-  // Redirige si admin es false
-  if (admin === false) {
-      router.push('/menu');
-  }
-}, [admin]);
+
+
+
   return (
-    <Suspense  fallback={<div>Loading...</div>}>
-      <DashboardAdmindV/>
+    <Suspense fallback={<div>Loading...</div>}>
+      <DashboardAdmindV />
     </Suspense>
   );
 };
