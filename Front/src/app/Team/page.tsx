7a12a9e1849bcde64782c2ba21de7845220e21fc
EmmaInverse <emma225h@gmail.com>
2024-11-06 12:08:17 -0300

    probando

---
+++ Front/src/app/Team/page.tsx
@@ -0,0 +1,14 @@
+
+import ViewTeam from '@/views/viewTeam/viewTeam';
+import React from 'react'
+
+const Team = () => {
+
+return (
+    <div>
+        <ViewTeam/>
+    </div>
+)
+}
+
+export default Team;
---
+++ Front/src/views/viewTeam/viewTeam.tsx
@@ -0,0 +1,13 @@
+
+import Team from '@/components/Team/Team';
+import React from 'react'
+
+const ViewTeam = () => {
+    return (
+        <div>
+            <Team />
+        </div>
+    )
+}
+
+export default ViewTeam;
